f7d5be0e28feb1811ed47b85bb615e6bc2fe86d8
Himanshu Kumar <himanshu064@gmail.com>
2025-05-01 07:45:29 +0530

    Added android configurations to build apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'webphone-ionic',
+  appName: 'Connexio Webphone',
   webDir: 'dist'
 };
 
